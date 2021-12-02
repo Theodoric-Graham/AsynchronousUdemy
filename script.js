8260dc3563b66e4da05a855fa63b7969c46d3849
Theodoric-Graham <theodoricgraham@hotmail.com>
2021-12-02 16:49:24 -0600

    Built a dynamic HTML element displaying info from the data object

--- script.js
+++ script.js
@@ -10,4 +10,29 @@ const countriesContainer = document.querySelector('.countries');
 
 //old way
 const request = new XMLHttpRequest();
-request.open('GET', '');
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+//Send off the request fetching the data
+request.send();
+
+//Once done it will emit the load event, triggering the callback function
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  //Copied from commented section
+  const html = `        <article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} Million people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
