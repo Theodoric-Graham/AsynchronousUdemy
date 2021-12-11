f85a89d4476ed73743123b18666a47298f789d3f
Theodoric-Graham <theodoricgraham@hotmail.com>
2021-12-11 13:58:08 -0600

    Added neighbour country functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const btnInput = document.getElementById('input-btn');
+const countriesContainer = document.querySelector('.countries');
 
 let input;
 
 //displaying the dynamic html
 const displayCountry = function (input) {
-  getCountryData(input);
+  getCountryAndNeighbour(input);
 };
 
 //Capturing the string
@@ -28,28 +29,9 @@ const enterKeyPressed = function (e) {
 };
 btnInput.addEventListener('click', countryString);
 
-const getCountryData = function (country) {
-  const btn = document.querySelector('.btn-country');
-  const countriesContainer = document.querySelector('.countries');
-
-  ///////////////////////////////////////
-  // https://restcountries.com/v2/
-
-  //Asynchronous Javascript and XML
-
-  //old way
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  //Send off the request fetching the data
-  request.send();
-
-  //Once done it will emit the load event, triggering the callback function
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    //Copied from commented section
-    const html = `        <article class="country">
+const renderCountry = function (data, className = '') {
+  //by adding className, and inserting neighbour, we get the styling from CSS for .neighbour
+  const html = `        <article class="country ${className}">
   <img class="country__img" src="${data.flag}" />
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -62,8 +44,45 @@ const getCountryData = function (country) {
   </div>
 </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const btn = document.querySelector('.btn-country');
+
+  ///////////////////////////////////////
+  // https://restcountries.com/v2/
+
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  //Send off the request fetching the data
+  request.send();
+
+  //Once done it will emit the load event, triggering the callback function
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Render Country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    //Send off the request fetching the data
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-getCountryData('usa');
+getCountryAndNeighbour('usa');
