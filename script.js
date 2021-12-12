1e72851975c050469d59890c1bb7baaf1d4dc7a9
Theodoric-Graham <theodoricgraham@hotmail.com>
2021-12-11 21:02:39 -0600

    added neighbour functionality using promises

--- script.js
+++ script.js
@@ -48,9 +48,22 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-// Getting data using fetch and promises
+//Getting the data using fetch and promises
 const getCountryData = function (country) {
+  //Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      console.log(data);
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+      //Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
+
+getCountryData('usa');
