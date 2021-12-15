bc7d841b42e714463c95b74a9b5018c39965282d
Theodoric-Graham <theodoricgraham@hotmail.com>
2021-12-14 21:19:39 -0600

    added coding challenge to pull from geocode api

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ btnInput.addEventListener('click', countryString);
 
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
+    //throwing an error inside the callback function of the then method
+    //immediately rejects the promise, traveling down the chain until it is 'caught'
     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
     return response.json();
@@ -87,7 +89,7 @@ const getCountryData = function (country) {
     //catching the error anywhere in the promise chain
     .catch(err => {
       console.error(`${err} ❌❌❌`);
-      //err.message is what we created in line 69
+      //err.message is what we created in line 65
       renderError(`Something went wrong ❌❌❌ ${err.message}. Try again!`);
     })
     //no matter if fulfilled or rejected, this will always be called
@@ -99,3 +101,69 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('usa');
 });
+
+//////Coding Challenge #1
+
+// In this challenge you will build a function 'whereAmI' which renders a country
+// only based on GPS coordinates. For that, you will use a second API to geocode
+// coordinates. So in this challenge, you’ll use an API on your own for the first time
+// Your tasks:
+
+// PART 1
+// 1. Create a function 'whereAmI' which takes as inputs a latitude value ('lat')
+// and a longitude value ('lng') (these are GPS coordinates, examples are in test
+// data below).
+// 2. Do “reverse geocoding” of the provided coordinates. Reverse geocoding means
+// to convert coordinates to a meaningful location, like a city and country name.
+// Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call
+// will be done to a URL with this format:
+// https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and
+// promises to get the data. Do not use the 'getJSON' function we created, that
+// is cheating
+// 3. Once you have the data, take a look at it in the console to see all the attributes
+// that you received about the provided location. Then, using this data, log a
+// message like this to the console: “You are in Berlin, Germany”
+// 4. Chain a .catch method to the end of the promise chain and log errors to the
+// console
+// 5. This API allows you to make only 3 requests per second. If you reload fast, you
+// will get this error with code 403. This is an error with the request. Remember,
+// fetch() does not reject the promise in this case. So create an error to reject
+// the promise yourself, with a meaningful error message
+
+// PART 2
+// 6. Now it's time to use the received data to render a country. So take the relevant
+// attribute from the geocoding API result, and plug it into the countries API that
+// we have been using.
+// 7. Render the country and catch any errors, just like we have done in the last
+// lecture (you can even copy this code, no need to type the same code)
+
+// Test data:
+// § Coordinates 1: 52.508, 13.381 (Latitude, Longitude)
+// § Coordinates 2: 19.037, 72.873
+// § Coordinates 3: -33.933, 18.474
+// GOOD LUCK
+
+////////////////////////////////////////////////////////////////
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Problem with geocoding ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      getCountryData(data.country);
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+    })
+    .catch(err => {
+      console.error(`${err} ❌❌❌`);
+      renderError(`Something went wrong ❌❌❌ ${err.message}. Try again!`);
+    });
+};
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
